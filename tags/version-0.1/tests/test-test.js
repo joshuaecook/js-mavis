d41ee03d145df8836b76671cb1758c26909bbd1c
ijoshua <ijoshua@0f3df26d-8130-0410-a0cb-d9e7f84b85d9>
2007-05-15 20:22:54 +0000

    0.1 tests for package-test

---
+++ tags/version-0.1/tests/test-test.js
@@ -0,0 +1,55 @@
+if ( 'undefined' === typeof test )
+  { throw "test-test depends on test package"  }
+
+var tests;
+if ( 'undefined' === typeof tests ) 
+  { tests = {}; }
+
+tests.test = (function ( runner )
+  {
+  // simulated compatibility with MochiTest
+  var is = function ( expected, actual, message )
+    {
+    var defaults = {printer: String, message: message  };
+    return test.assertEqual(expected,actual,defaults);
+    };
+  var ok = function ( value, message )
+    {
+    return test.assertTrue(message,value);
+    };
+
+  var fix = function ( f )
+    {
+    var setup = function ()
+      {
+      var T = function ( value ) { this.value = value;  };
+      T.prototype.func = function () { return this.value;  };
+      return T;
+      };
+    var teardown = function () {};
+
+    return test.fixture(setup,teardown,f);
+    };
+  
+  var l = test.label;
+  var s = test.suite;
+  var t = test.testcase;
+
+  return l("boring package",s(
+    l("boring",t(fix(function (T)
+      {
+      var o = new T("boring");
+      ok(o.func instanceof Function,"func should be a Function");
+      is("boring",o.func(),"func() should be 'boring'");
+      }))),
+    l("boring2",t(function ()
+      {
+      var ary = ['a','b','c'];
+      is(3,ary.length,"length should be 3");
+      })),
+    l("fail",t(function ()
+      {
+      test.fail("this should fail")
+      }))
+    ));
+  })();
